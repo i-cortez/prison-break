ef817541e19184b698f45269adb978599bdf726f
Izze <56184561+i-cortez@users.noreply.github.com>
2020-12-04 22:35:17 -0800

    Added inventory demo scene

---
+++ Assets/Src/InventoryUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+   public GameObject inventoryUI;
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+   
+   // Update is called once per frame
+   void Update()
+   {
+      if (Input.GetButtonDown("Inventory"))
+      {
+         inventoryUI.SetActive(!inventoryUI.activeSelf);
+      }
+
+   }
+}
+
